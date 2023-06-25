d577f6cc7fd9ed4e49437964af7ddedfd5a365c2
3REAPER <pervukhin-nikitka@mail.ru>
2023-06-25 12:53:49 +0600

    rename table

--- app/src/main/java/ru/pervukhin/pizzashop/di/RoomModule.kt
+++ app/src/main/java/ru/pervukhin/pizzashop/di/RoomModule.kt
@@ -2,12 +2,15 @@ package ru.pervukhin.pizzashop.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Module
 import dagger.Provides
 import ru.pervukhin.pizzashop.data.database.AppDataBase
 import ru.pervukhin.pizzashop.data.database.CartDishDao
 import ru.pervukhin.pizzashop.data.database.DishDao
 import javax.inject.Singleton
+
 
 @Module
 class RoomModule(private val context: Context) {
@@ -27,6 +30,6 @@ class RoomModule(private val context: Context) {
     @Singleton
     @Provides
     fun provideAppDataBase(): AppDataBase {
-        return Room.databaseBuilder(context, AppDataBase::class.java,"dataBase.db").build()
+        return Room.databaseBuilder(context, AppDataBase::class.java,"db.db").build()
     }
 }
